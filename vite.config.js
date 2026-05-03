039b8da9bf00fbaecb9a9d59c5a19e3f12cd96f3
academialuchorolandopadel <abcconstruccionesenseco@gmail.com>
2026-05-03 19:21:27 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Cambiá "padel-app" por el nombre exacto de tu repositorio en GitHub
-export default defineConfig({
-  plugins: [react()],
-  base: "/padel-app/",   // ← debe coincidir con el nombre del repo
-});
